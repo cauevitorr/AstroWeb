b4a966e79147dbb5c5a00c56ad39c52b7943336e
Cauê Vitor <138812443+cauevitorr@users.noreply.github.com>
2024-05-20 18:32:40 -0300

    Add files via upload

---
+++ src/Styles/App.js
@@ -0,0 +1,20 @@
+// import { styled } from "styled-components";
+
+
+// export const Header = styled.header`
+// display: flex;
+// align-items: center;
+// justify-content: space-around;
+// `
+// export const Nav = styled.nav`
+// display: flex;
+// `
+
+// export const H2 = styled.h2`
+// font-family: "Orbitron", sans-serif;
+//  font-optical-sizing: auto;
+//  font-weight: 400;
+//  font-style: normal;
+//  color: white;
+//  margin-right: 10px;
+// `
